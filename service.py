6f5ec9d7c37ea90b133eb480645ffc9569c57d9e
ebrockainq <112428189+ebrockainq@users.noreply.github.com>
2025-08-28 14:01:15 -0400

    Fix emailStr import

--- service.py
+++ service.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, EmailStr
 import datetime
 from sqlalchemy import (
     create_engine, Table, Column, String, Integer, Boolean,
